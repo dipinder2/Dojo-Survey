901861f5665aa44284335621a7ac5fcdf470d118
Dipinder Sharma <dipinder2@gmail.com>
2021-06-11 09:09:18 -0700

    first commit

--- server.py
+++ server.py
@@ -9,12 +9,17 @@ def index():
     
 @app.route('/results', methods=['POST'])
 def results():
+    favpets = request.form.getlist('favorite_pet')
+    fav = ",".join(favpets)
     userDict = {
       'name':request.form['name'],
       'location': request.form['location'],
       'favLang':request.form['favLang'],
-      'comments':request.form['comments']
+      'gender':request.form['gender'],
+      'favorite_pet':fav,
+      'comments':request.form['comments'],
     }
+    print(userDict["favorite_pet"])
     print(userDict)
     return render_template('results.html',userDict=userDict)
 
